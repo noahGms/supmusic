0d6b9ef48cf2aaf28e0481f2396b9e767c8ef744
noahGms <noah@keshin.net>
2022-02-13 14:15:27 +0100

    moved logic of color mode swticher in specific file

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -161,22 +161,3 @@ function updateSongInProgressText(name) {
 function getNameWithoutExtension(name) {
     return name.replace('.mp3', '');
 }
-
-function toggleDarkMode() {
-    halfmoon.toggleDarkMode();
-    updateToggleDarkModeSwitch();
-}
-
-function updateToggleDarkModeSwitch() {
-    if (halfmoon.readCookie("halfmoon_preferredMode")) {
-        if (halfmoon.readCookie("halfmoon_preferredMode") === "light-mode") {
-            $('#toggleDarkModeInput').prop("checked", false);
-            $('#toggleDarkModeLabel').html(`<i class="fas fa-sun"></i>`);
-        }
-        else if (halfmoon.readCookie("halfmoon_preferredMode") === "dark-mode") {
-            $('#toggleDarkModeInput').prop("checked", true);
-            $('#toggleDarkModeLabel').html(`<i class="fas fa-moon"></i>`);
-        }
-    }
-}
-updateToggleDarkModeSwitch();
