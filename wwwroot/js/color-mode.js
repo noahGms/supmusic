818a1fc7a00976be5c848d539a360ce96401c97e
noahGms <noah@keshin.net>
2022-02-14 14:26:19 +0100

    moved color mode switcher from bottom to navbar

--- wwwroot/js/color-mode.js
+++ wwwroot/js/color-mode.js
@@ -6,12 +6,10 @@ function toggleDarkMode() {
 function updateToggleDarkModeSwitch() {
     if (halfmoon.readCookie("halfmoon_preferredMode")) {
         if (halfmoon.readCookie("halfmoon_preferredMode") === "light-mode") {
-            $('#toggleDarkModeInput').prop("checked", false);
-            $('#toggleDarkModeLabel').html(`<i class="fas fa-sun"></i>`);
+            $('#toggleDarkModeBtn').html(`<i class="fas fa-sun"></i>`);
         }
         else if (halfmoon.readCookie("halfmoon_preferredMode") === "dark-mode") {
-            $('#toggleDarkModeInput').prop("checked", true);
-            $('#toggleDarkModeLabel').html(`<i class="fas fa-moon"></i>`);
+            $('#toggleDarkModeBtn').html(`<i class="fas fa-moon"></i>`);
         }
     }
 }
